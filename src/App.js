a6dc149db92e26a7da921b7e0949eb43c9460299
Saurabh Chauhan <imdsaurabh30@gmail.com>
2024-08-16 11:24:47 -0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker, Polyline } from '@react-google-maps/api';
+
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.0060,
+};
+
+const path = [
+  { lat: 40.7128, lng: -74.0060 },
+  { lat: 40.7138, lng: -74.0050 },
+  { lat: 40.7148, lng: -74.0040 },
+];
 
 function App() {
+  const [vehiclePosition, setVehiclePosition] = useState(center);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVehiclePosition(path[index]);
+      setIndex((prevIndex) => (prevIndex + 1) % path.length);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyCFHKAhNums74mufitfxwzOV1GeAFczqQg">
+      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={15}>
+        <Marker position={vehiclePosition} />
+        <Polyline path={path} />
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
